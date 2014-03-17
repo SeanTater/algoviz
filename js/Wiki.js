bde30a668a724231447bf5bc44d79c88a6ff2abc
Sean Gallagher <stgallag@gmail.com>
2014-03-17 16:04:28 -0400

    Wikipedia crawling functionality complete

--- js/Wiki.js
+++ js/Wiki.js
@@ -1,34 +1,59 @@
 Wiki = {
   /** Parse and setup wiki runner */
   init: function(text) {
-    Wiki.tokens = text.match(/\w+/g);
-    $("#logwindow").empty()
-    setTimeout(function(){
-      Wiki.run([
-        new Stat("Builtin Object", Wiki.hist_builtin, 800, 51200),
-        new Stat("Unbalanced Binary Search Tree", Wiki.hist_bst, 800, 51200),
-        new Stat("Hashtable", Wiki.hist_hash, 800, 51200),
-        new Stat("Sorted Array", Wiki.hist_sorted_array, 800, 51200),
-      ]);
-    }, 0);
-  },
-  
+    $("#logwindow").append("<br/>Collecting documents from wikipedia..<br/>");
+    Wiki.crawl_wikipedia(["Goose"], 5, function() {
+      // When finished crawling wikipedia
+      $("#logwindow").append("Finished collecting " + Articles.all.length + " documents.<br/>");
+      setTimeout(function(){
+        Wiki.get_histograms([
+          new Stat("Builtin Object", Wiki.hist_builtin, 800, 51200),
+          new Stat("Unbalanced Binary Search Tree", Wiki.hist_bst, 800, 51200),
+          new Stat("Hashtable", Wiki.hist_hash, 800, 51200),
+          new Stat("Sorted Array", Wiki.hist_sorted_array, 800, 51200),
+        ]); // get_histograms()
+      }, 0); // setTimeout()
+    }); // crawl_wikipedia()
+  }, // init()
+
+  /** Crawl up to `limit` pages of Wikipedia articles, starting at `names`.
+    * Names is an array of article names, and the crawl is a BFS.
+    */
+  crawl_wikipedia: function(names, limit, done) {
+    var next_names = [];
+    Articles.fetch(names, function(article){ // for each article
+      if (next_names.indexOf(article.title) < 0) {
+        next_names = next_names.concat(article.links);
+      }
+      limit--;
+    }, function() { // at the end of the fetch
+      if (limit > 0) { // Trigger the next level of the BFS
+        setTimeout(function(){
+          Wiki.crawl_wikipedia(next_names.slice(0, limit), limit, done);
+        }, 0); 
+      } else {
+        done();
+      }
+    }); // Articles.fetch
+  }, // crawl_wikipedia
+
+
   /** Rerun a performance test against a queue of datasets
    * Each dataset is run 10 times and averaged
    * If it has not met the size cap, it is doubled
    * If it has met the cap, it moves to the next entry or schedules for charts
    *   to be drawn.
    */
-  run: function(q) {
+  get_histograms: function(q) {
     // Setup this entry in the queue
     e = q[0]
     // You can't index more tokens than you have
-    e.count = Math.min(e.count, Wiki.tokens.length);
+    e.count = Math.min(e.count, Articles.all_tokens.length);
     
     // Run the performance tests 10 times
     var run_time = new Date();
     for(var i=0; i<3; i++) {
-      e.benchmark( Wiki.tokens.slice(0, e.count) );
+      e.benchmark( Articles.all_tokens.slice(0, e.count) );
     }
     run_time = (new Date() - run_time)/3;
     
@@ -38,14 +63,14 @@ Wiki = {
     e.costs.push(run_time/e.count);
     
     // What to do after this function?
-    if (e.count < Math.min(e.cap, Wiki.tokens.length)) {
+    if (e.count < Math.min(e.cap, Articles.all_tokens.length)) {
       // Spawn another test
       e.count *= 2;
-      setTimeout(function(){ Wiki.run(q) }, 0);
+      setTimeout(function(){ Wiki.get_histograms(q) }, 0);
     } else if (q.length > 1) {
       // Finish this group and test a new benchmark
       q.shift();
-      setTimeout(function(){ Wiki.run(q) }, 0);
+      setTimeout(function(){ Wiki.get_histograms(q) }, 0);
     } else {
       // Display the results
       setTimeout(Stats.display, 0);
@@ -95,8 +120,5 @@ Wiki = {
     tokens.forEach(arr.count, arr);
   }
 };
-Wiki.preferred_method = Wiki.hist_bst;
 
-$(function() {
-  $.get("https://googledrive.com/host/0B8wOEC5-v5lXTTg2cUZwQzJ6cWs/wiki-100m.txt", Wiki.init);
-});
+$(Wiki.init);
