49b11870abc5b45fa553c5d57cbefc7dcc0682a3
Sean Gallagher <stgallag@gmail.com>
2014-04-17 09:46:34 -0400

    Clean up Benchmark names

--- js/Stat.js
+++ js/Stat.js
@@ -8,8 +8,10 @@ function Stat(name, benchmark) {
    
    // This is called before the DOM is finished. So call this later.
    $(function(){
-        $("#benchmark_settings").append('<input type="checkbox" name="sb on hm" value="sb on hm" checked="true" />'
-            + '<label for="sb on hm">' + benchmark.constructor.name + ' on ' + benchmark.subject_name + '</label><br />');
+       var benchmark_name = benchmark.constructor.name.replace("Benchmark", "");
+       var map_name = benchmark.subject_name.replace("Map", "");
+       $("#benchmark_settings").append('<input type="checkbox" name="sb on hm" value="sb on hm" checked="true" />'
+           + '<label for="sb on hm">' + benchmark_name + ' ' + map_name + '</label><br />');
    });
 }
 
