affd034bb7392a8a6a66784a366b41841ae3dbc1
Sean Gallagher <stgallag@gmail.com>
2014-04-17 05:41:43 -0400

    Create Benchmark options

--- js/Stat.js
+++ js/Stat.js
@@ -9,7 +9,7 @@ function Stat(name, benchmark) {
    // This is called before the DOM is finished. So call this later.
    $(function(){
         $("#benchmark_settings").append('<input type="checkbox" name="sb on hm" value="sb on hm" checked="true" />'
-            + '<label for="sb on hm">' + benchmark.name + ' on ' + HashtableMap.name + '</label><br />');
+            + '<label for="sb on hm">' + benchmark.constructor.name + ' on ' + benchmark.subject_name + '</label><br />');
    });
 }
 
